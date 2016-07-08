5bebc7efeaa7192239f1ce1737b06844e3f31998
Remi Michel <remi.michel38@gmail.com>
2016-07-08 06:32:23 +0000

    finish timestamp exercise

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require('express');
+const utils = require('./utils');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({status: 'OK'});
+});
+
+app.get('/:dateStr', function (req, res) {
+  let natural = req.params.dateStr;
+  let unix = null;
+  const timestamp = utils.toTimestamp(natural);
+  (!isNaN(timestamp)) ? unix = timestamp : natural = null;
+  
+  res.send({unix, natural});
 });
 
 app.listen(8080, function () {
---
+++ utils.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    toTimestamp: (dateStr) => Date.parse(dateStr),
+};
